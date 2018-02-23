78aae389e72ef59b27858e0776e56af46e52fdba
Zachary Sunberg <sunbergzach@gmail.com>
2018-02-23 13:43:37 -0800

    added quadrant tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -53,3 +53,33 @@ for quadrant in [Vec2(1,1), Vec2(-1,1), Vec2(1,-1), Vec2(-1,-1)]
         end
     end
 end
+
+# make sure it does pass through walls sometimes without any barriers
+pomdp = VDPTagPOMDP()
+N = 100
+for quadrant in [Vec2(1,1), Vec2(-1,1), Vec2(1,-1), Vec2(-1,-1)]
+    in_other = falses(N)
+    @showprogress for i in 1:N
+        is = initial_state(pomdp, Base.GLOBAL_RNG)
+        is = TagState(quadrant, is.target)
+        hr = HistoryRecorder(max_steps=100)
+        hist = simulate(hr, pomdp, ToNextML(pomdp), filter)
+        in_other[i] = any(any(s.agent.*quadrant .< 0.0) for s in state_hist(hist))
+    end
+    @show sum(in_other)/length(in_other)
+    println("Should be near one?")
+    @test any(in_other)
+end
+for quadrant in [Vec2(1,1), Vec2(-1,1), Vec2(1,-1), Vec2(-1,-1)]
+    in_other = falses(N)
+    @showprogress for i in 1:N
+        is = initial_state(pomdp, Base.GLOBAL_RNG)
+        is = TagState(quadrant, is.target)
+        hr = HistoryRecorder(max_steps=2)
+        hist = simulate(hr, pomdp, RandomPolicy(pomdp))
+        in_other[i] = any(any(s.agent.*quadrant .< 0.0) for s in state_hist(hist))
+    end
+    @show sum(in_other)/length(in_other)
+    println("Should be near 3/4?")
+    @test any(in_other)
+end
